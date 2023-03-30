1684bd93edc0a9a336f42678794c93fc7a908ef5
Julia Cunha <juliacunha@julias-air.lan>
2023-03-29 21:21:32 -0400

    mar29

---
+++ 03.29.2023.js
@@ -0,0 +1,13 @@
+//Sum Mixed Array
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+//Return your answer as a number.
+
+function sumMix(x){
+  let sum = 0
+  let num = x.map(y => Number(y)) 
+     for(let i = 0; i < num.length; i++){
+       sum += num[i]
+     }
+    return sum
+  }
